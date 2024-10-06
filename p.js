df4c7561d2167a3299eab85e6364b4a56fb7dfad
choco-chi <kingellico@gmail.com>
2024-10-06 18:22:17 +0700

    Add files via upload

---
+++ p.js
@@ -0,0 +1,4 @@
+// Simple script to simulate uploading images for portfolio updates
+document.querySelector('#upload-btn').addEventListener('click', function() {
+    alert('Upload new image feature is coming soon!');
+});
